8b312e8f56b7f915a5f83f8e46bdc437f21c3531
Anthony Ivins <ivinsant@gmail.com>
2024-03-12 06:02:14 +0000

    adding files

---
+++ Crew Test/main.py
@@ -0,0 +1,81 @@
+import os
+from crewai import Agent, Task, Crew, Process
+from crewai_tools import SerperDevTool
+
+# os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
+os.environ["SERPER_API_KEY"] = "f65059662cfe5b21e900ae31fb29a5424ecf9b1a" # serper.dev API key
+
+# You can choose to use a local model through Ollama for example. See https://docs.crewai.com/how-to/LLM-Connections/ for more information.
+
+os.environ["OPENAI_API_BASE"] = 'http://localhost:1234/v1'
+# os.environ["OPENAI_MODEL_NAME"] ='openhermes'  # Adjust based on available model
+# os.environ["OPENAI_API_KEY"] ='sk-111111111111111111111111111111111111111111111111'
+
+# OPENAI_API_BASE="http://localhost:1234/v1"
+# OPENAI_MODEL_NAME=NA
+# OPENAI_API_KEY=NA
+
+
+search_tool = SerperDevTool()
+
+# Define your agents with roles and goals
+researcher = Agent(
+  role='Senior Research Analyst',
+  goal='Uncover cutting-edge developments in Cybersecurity',
+  backstory="""You work at a leading cybersecurity company.
+  Your expertise lies in identifying emerging trends.
+  You have a knack for dissecting complex data and presenting actionable insights.""",
+  verbose=True,
+  allow_delegation=False,
+  tools=[search_tool]
+  # You can pass an optional llm attribute specifying what mode you wanna use.
+  # It can be a local model through Ollama / LM Studio or a remote
+  # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
+  #
+  # import os
+  # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
+  #
+  # OR
+  #
+  # from langchain_openai import ChatOpenAI
+  # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
+)
+writer = Agent(
+  role='Tech Content Strategist',
+  goal='Craft compelling content on penetrang',
+  backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
+  You transform complex concepts into compelling narratives that everybody can understand.""",
+  verbose=True,
+  allow_delegation=True
+)
+
+# Create tasks for your agents
+task1 = Task(
+  description="""Conduct a comprehensive analysis of the latest threats in ransomware in 2024.
+  Identify key trends, emerging threats, and potential industry impacts.""",
+  expected_output="Full analysis report in bullet points",
+  agent=researcher
+)
+
+task2 = Task(
+  description="""Using the insights provided, develop an engaging blog
+  post that highlights the most significant ransomware attacks and threats.
+  Your post should be informative yet accessible, catering to a tech-savvy audience.
+  Make it sound cool, avoid complex words so it doesn't sound like AI and the common person
+  can understand. """,
+  expected_output="Full blog post of at least 6 paragraphs",
+  agent=writer
+)
+
+# Instantiate your crew with a sequential process
+crew = Crew(
+  agents=[researcher, writer],
+  tasks=[task1, task2],
+  verbose=2, # You can set it to 1 or 2 to different logging levels
+)
+
+# Get your crew to work!
+result = crew.kickoff()
+
+print("######################")
+print(result)
